b4cb300338f97255908c301cde7b6d105cf41aa9
Emil Majer <emil.majer@strix.net>
2022-09-11 12:04:16 +0200

    Lint project

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,13 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -16,7 +22,6 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
-
   <RouterView />
 </template>
 
